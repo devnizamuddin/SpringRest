fc7caad66fe77b1d8e190cd6ff568d071340caed
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-28 02:48:01 +0600

    few sample course added and return from couse imp class

--- src/main/java/com/nizam/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/nizam/springrest/services/CourseServiceImpl.java
@@ -2,11 +2,22 @@ package com.nizam.springrest.services;
 
 import com.nizam.springrest.entities.Course;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CourseServiceImpl implements CourseService{
+
+    List<Course>courseList;
+    public CourseServiceImpl() {
+        courseList = new ArrayList<>();
+        courseList.add(new Course(324,"Bangla","Learning Bangla"));
+        courseList.add(new Course(3423,"English","Learning English"));
+        courseList.add(new Course(23545,"Math","Learning Math"));
+        courseList.add(new Course(345345,"Science","Learning Science"));
+    }
+
     @Override
     public List<Course> getCourse() {
-        return null;
+        return courseList;
     }
 }
