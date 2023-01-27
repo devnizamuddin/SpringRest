91022e3167d3568c049f44a390f10d323696c4fa
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-28 02:58:55 +0600

    get courses api created

--- src/main/java/com/nizam/springrest/controller/CourseController.java
+++ src/main/java/com/nizam/springrest/controller/CourseController.java
@@ -1,4 +1,20 @@
 package com.nizam.springrest.controller;
 
+import com.nizam.springrest.entities.Course;
+import com.nizam.springrest.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+    @GetMapping("/getCourses")
+    List<Course>getCourses(){
+        return courseService.getCourses();
+    }
 }
