e88fbd00907aa7c16f92947590a30609e8990616
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-28 02:16:44 +0600

    get greetings api created

--- src/main/java/com/nizam/springrest/GreetingController.java
+++ src/main/java/com/nizam/springrest/GreetingController.java
@@ -1,2 +1,13 @@
-package com.nizam.springrest;public class GreetingController {
+package com.nizam.springrest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/getGreetings")
+    public String getGreetings(){
+
+        return "Assalamualikum";
+    }
 }
