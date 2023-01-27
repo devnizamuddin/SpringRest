3ae7feb7ce32ec58657c7daf0efc19fd76488505
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-28 02:27:28 +0600

    id, name, description field added in course entity

--- src/main/java/com/nizam/springrest/entities/Course.java
+++ src/main/java/com/nizam/springrest/entities/Course.java
@@ -1,4 +1,37 @@
 package com.nizam.springrest.entities;
 
 public class Course {
+    long id;
+    String name;
+    String description;
+
+    public Course(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
