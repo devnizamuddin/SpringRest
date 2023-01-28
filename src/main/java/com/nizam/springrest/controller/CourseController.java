c34979d87b2645728b9ce42371d95fcf69e55a69
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-29 02:30:34 +0600

    Long to long in getcourse api

--- src/main/java/com/nizam/springrest/controller/CourseController.java
+++ src/main/java/com/nizam/springrest/controller/CourseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CourseController {
@@ -19,7 +20,7 @@ public class CourseController {
     }
 
     @GetMapping("/getCourse/{courseId}")
-    Course getCourse(@PathVariable Long courseId) {
+    Optional<Course> getCourse(@PathVariable long courseId) {
         return courseService.getCourse(courseId);
     }
 
