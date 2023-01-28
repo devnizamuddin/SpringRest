59b7e083a66f88cfbad8476049fa27d0aacabe9f
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-29 02:34:14 +0600

    get course api updated

--- src/main/java/com/nizam/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/nizam/springrest/services/CourseServiceImpl.java
@@ -7,19 +7,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class CourseServiceImpl implements CourseService{
+public class CourseServiceImpl implements CourseService {
 
     @Autowired
     CourseDao courseDao;
-    List<Course>courseList;
+    List<Course> courseList;
+
     public CourseServiceImpl() {
         courseList = new ArrayList<>();
-        courseList.add(new Course(2,"Bangla","Learning Bangla"));
-        courseList.add(new Course(3,"English","Learning English"));
-        courseList.add(new Course(4,"Math","Learning Math"));
-        courseList.add(new Course(5,"Science","Learning Science"));
+        courseList.add(new Course(2, "Bangla", "Learning Bangla"));
+        courseList.add(new Course(3, "English", "Learning English"));
+        courseList.add(new Course(4, "Math", "Learning Math"));
+        courseList.add(new Course(5, "Science", "Learning Science"));
     }
 
     @Override
@@ -28,15 +30,10 @@ public class CourseServiceImpl implements CourseService{
     }
 
     @Override
-    public Course getCourse(long id) {
-        Course course = null;
-        for (Course course1: courseList){
-            if (course1.getId()==id){
-                course = course1;
-                break;
-            }
-        }
-        return course;
+    public Optional<Course> getCourse(long id) {
+
+        return courseDao.findById(Long.parseLong(String.valueOf(id)));
+
     }
 
     @Override
@@ -48,9 +45,9 @@ public class CourseServiceImpl implements CourseService{
     @Override
     public Course updateCourse(Course updatedCourse) {
         Course course = null;
-        for (int i =0;i<courseList.size();i++){
-            if (updatedCourse.getId()==courseList.get(i).getId()){
-                courseList.set(i,updatedCourse);
+        for (int i = 0; i < courseList.size(); i++) {
+            if (updatedCourse.getId() == courseList.get(i).getId()) {
+                courseList.set(i, updatedCourse);
                 course = updatedCourse;
             }
         }
@@ -60,8 +57,8 @@ public class CourseServiceImpl implements CourseService{
     @Override
     public String deleteCourse(long id) {
 
-        for (Course course : courseList){
-            if (course.getId()==id){
+        for (Course course : courseList) {
+            if (course.getId() == id) {
                 courseList.remove(course);
                 return "Successfully deleted";
             }
