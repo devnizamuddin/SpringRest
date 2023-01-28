ac437b72ee0494057c00bcc6147fc3095d6350e5
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-29 01:38:08 +0600

    course dao interface added with exted jpa repo

--- src/main/java/com/nizam/springrest/dao/CourseDao.java
+++ src/main/java/com/nizam/springrest/dao/CourseDao.java
@@ -1,2 +1,7 @@
-package com.nizam.springrest.dao;public class CourseDao {
+package com.nizam.springrest.dao;
+
+import com.nizam.springrest.entities.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseDao extends JpaRepository<Course, Long> {
 }
