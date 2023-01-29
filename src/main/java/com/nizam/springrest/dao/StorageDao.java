efe91d64ba5fe00ab1daebcf3d1ce8e4f766d955
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-29 18:06:38 +0600

    storage dao created for download and upload image

--- src/main/java/com/nizam/springrest/dao/StorageDao.java
+++ src/main/java/com/nizam/springrest/dao/StorageDao.java
@@ -1,2 +1,13 @@
-package com.nizam.springrest.dao;public class StorageDao {
+package com.nizam.springrest.dao;
+
+import com.nizam.springrest.entities.ImageData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface  StorageDao extends JpaRepository<ImageData,Long> {
+
+    Optional<ImageData>findByName(String fileName);
+
+    byte[] downloadImage(String fileName);
 }
