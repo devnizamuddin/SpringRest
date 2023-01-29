66a7bbc06b9d12a75e4217fe4918af2f4398cce5
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-29 18:05:50 +0600

    storate service imp added for download and upload image

--- src/main/java/com/nizam/springrest/services/StorageServiceImpl.java
+++ src/main/java/com/nizam/springrest/services/StorageServiceImpl.java
@@ -1,2 +1,36 @@
-package com.nizam.springrest.services;public class StorageServiceImpl {
+package com.nizam.springrest.services;
+
+import com.nizam.springrest.dao.StorageDao;
+import com.nizam.springrest.entities.ImageData;
+import com.nizam.springrest.utils.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class StorageServiceImpl implements  StorageService{
+
+    @Autowired
+    StorageDao storageDao;
+
+    @Override
+    public String uploadImage(MultipartFile file) throws IOException {
+       ImageData imageData =  storageDao.save(ImageData.builder().name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtils.compressImage(file.getBytes())).build());
+       if (imageData!=null){
+           return "File uploaded successfully : "+file.getOriginalFilename();
+       }
+        return null;
+    }
+
+    @Override
+    public byte[] downloadImage(String fileName){
+        Optional<ImageData> dbImageData = storageDao.findByName(fileName);
+        byte[]images = ImageUtils.decompressImage(dbImageData.get().getImageData());
+        return images;
+    }
 }
