6a9791ecf3f1b89d036ee6b4989be00dcbd13018
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-29 18:05:13 +0600

    storage service created for download and upload image

--- src/main/java/com/nizam/springrest/services/StorageService.java
+++ src/main/java/com/nizam/springrest/services/StorageService.java
@@ -1,2 +1,12 @@
-package com.nizam.springrest.services;public interface StorageService {
+package com.nizam.springrest.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface StorageService {
+
+    String uploadImage(MultipartFile file) throws IOException;
+
+    byte[] downloadImage(String fileName);
 }
