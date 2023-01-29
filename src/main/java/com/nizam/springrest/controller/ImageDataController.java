cd020d91be0c9c0bf68cbfd7628fcc8bd9659052
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-29 18:07:23 +0600

    ImageController and upload and download api created

--- src/main/java/com/nizam/springrest/controller/ImageDataController.java
+++ src/main/java/com/nizam/springrest/controller/ImageDataController.java
@@ -1,2 +1,37 @@
-package com.nizam.springrest.controller;public class ImageDataController {
+package com.nizam.springrest.controller;
+
+import com.nizam.springrest.services.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequestMapping("/image")
+public class ImageDataController {
+
+    @Autowired
+    StorageService storageService;
+
+    @PostMapping
+    public ResponseEntity<?> uploadImage(@RequestParam("image")MultipartFile file) throws IOException {
+
+        String uploadImage = storageService.uploadImage(file);
+
+        return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
+    }
+
+    public ResponseEntity<?>downloadImage(@PathVariable String fileName){
+
+        byte[] imageData = storageService.downloadImage(fileName);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png")).body(imageData);
+    }
 }
