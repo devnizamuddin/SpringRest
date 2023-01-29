b686bdf89877b297731d48cfb127b6a67bd79028
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-29 18:02:38 +0600

    Image data entity created

--- src/main/java/com/nizam/springrest/entities/ImageData.java
+++ src/main/java/com/nizam/springrest/entities/ImageData.java
@@ -1,2 +1,25 @@
-package com.nizam.springrest.entities;public class ImageData {
+package com.nizam.springrest.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ImageData")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+    @Lob
+    @Column(name = "imagedata")
+    private byte[] imageData;
 }
