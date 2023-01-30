d9084fc8e6d6b2483e11e627e86a8c17d1ef391f
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-31 02:23:31 +0600

    file entity added with property

--- src/main/java/com/nizam/springrest/entities/FileData.java
+++ src/main/java/com/nizam/springrest/entities/FileData.java
@@ -1,2 +1,22 @@
-package com.nizam.springrest.entities;public class FileData {
+package com.nizam.springrest.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "FileData")
+@Data
+@Builder
+public class FileData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String type;
+    String location;
 }
