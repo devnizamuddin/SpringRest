75c409155e650eea7ed66cad4797e77eb49e2a88
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-31 02:21:43 +0600

    upload file api added

--- src/main/java/com/nizam/springrest/controller/FileDataController.java
+++ src/main/java/com/nizam/springrest/controller/FileDataController.java
@@ -1,2 +1,24 @@
-package com.nizam.springrest.controller;public class FileDataController {
+package com.nizam.springrest.controller;
+
+import com.nizam.springrest.services.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController("/file")
+public class FileDataController {
+
+    @Autowired
+    FileService fileService;
+
+
+    @PostMapping()
+    String fileUpload(@RequestParam MultipartFile file) throws IOException {
+       return fileService.uploadFile(file);
+    }
+
 }
