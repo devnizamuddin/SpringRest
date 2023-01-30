e8afc8a08c11f8cde2032471060295168a022718
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-31 02:21:14 +0600

    some anotation added for reading

--- src/main/java/com/nizam/springrest/SpringRestApplication.java
+++ src/main/java/com/nizam/springrest/SpringRestApplication.java
@@ -2,6 +2,9 @@ package com.nizam.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class SpringRestApplication {
@@ -11,3 +14,14 @@ public class SpringRestApplication {
 	}
 
 }
+
+
+/*
+* @ComponentScan({"com.nizam.springrest.services"})
+@EntityScan("com.nizam.springrest.entity")
+@EnableJpaRepositories("com.nizam.springrest.dao")
+*
+* @ComponentScan({"com.delivery.request"})
+@EntityScan("com.delivery.domain")
+@EnableJpaRepositories("com.delivery.repository")
+* */
