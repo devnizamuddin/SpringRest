a0191e814291217fa066861da57020b091f25c5f
nizamuddinshamrat <nshamrat7@gmail.com>
2023-01-31 02:22:51 +0600

    file dao added with jpa to upload and download image

--- src/main/java/com/nizam/springrest/dao/FileDao.java
+++ src/main/java/com/nizam/springrest/dao/FileDao.java
@@ -1,2 +1,7 @@
-package com.nizam.springrest.dao;public class FileDao {
+package com.nizam.springrest.dao;
+
+import com.nizam.springrest.entities.FileData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileDao extends JpaRepository<FileData,Long> {
 }
