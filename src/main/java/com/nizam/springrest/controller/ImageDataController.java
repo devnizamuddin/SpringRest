5a87913cf85f642d49c2f3fae8c595239de32462
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-30 16:56:16 +0600

    image controller set as rest controller and get mapping added in download image

--- src/main/java/com/nizam/springrest/controller/ImageDataController.java
+++ src/main/java/com/nizam/springrest/controller/ImageDataController.java
@@ -5,14 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@RestController
 @RequestMapping("/image")
 public class ImageDataController {
 
@@ -27,6 +25,7 @@ public class ImageDataController {
         return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
     }
 
+    @GetMapping("/{fileName}")
     public ResponseEntity<?>downloadImage(@PathVariable String fileName){
 
         byte[] imageData = storageService.downloadImage(fileName);
