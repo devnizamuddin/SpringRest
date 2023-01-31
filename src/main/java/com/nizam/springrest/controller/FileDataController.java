22752e6f3243f7046d7c86285ea41bb2a58f2c41
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-31 15:40:08 +0600

    anotation upload to solved issue

--- src/main/java/com/nizam/springrest/controller/FileDataController.java
+++ src/main/java/com/nizam/springrest/controller/FileDataController.java
@@ -2,23 +2,29 @@ package com.nizam.springrest.controller;
 
 import com.nizam.springrest.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RestController("/file")
+@RestController
+@RequestMapping("/file")
 public class FileDataController {
 
     @Autowired
     FileService fileService;
 
+    @PostMapping
+    public ResponseEntity<?> fileUpload(@RequestParam("file")MultipartFile file) throws IOException {
 
-    @PostMapping()
-    String fileUpload(@RequestParam MultipartFile file) throws IOException {
-       return fileService.uploadFile(file);
+        String uploadImage = fileService.uploadFile(file);
+
+        return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
     }
 
 }
