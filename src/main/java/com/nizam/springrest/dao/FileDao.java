6a31b198e5de07bc23c292b2ae96498d9420a58e
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-01-31 16:44:33 +0600

    find by name function added

--- src/main/java/com/nizam/springrest/dao/FileDao.java
+++ src/main/java/com/nizam/springrest/dao/FileDao.java
@@ -3,5 +3,8 @@ package com.nizam.springrest.dao;
 import com.nizam.springrest.entities.FileData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FileDao extends JpaRepository<FileData,Long> {
+    Optional<FileData>findByName(String name);
 }
