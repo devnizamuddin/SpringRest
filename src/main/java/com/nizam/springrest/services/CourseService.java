e069d0513b6e3bef554219497777c7179930c29f
nizamuddinshamrat <nshamrat7@gmail.com>
2023-04-24 02:25:46 +0600

    course service interface removed

--- src/main/java/com/nizam/springrest/services/CourseService.java
+++ src/main/java/com/nizam/springrest/services/CourseService.java
@@ -1,18 +1,56 @@
 package com.nizam.springrest.services;
 
+import com.nizam.springrest.dao.CourseDao;
 import com.nizam.springrest.entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CourseService {
-    List<Course> getCourses();
+@Service
+public class CourseService {
 
-    Optional<Course> getCourse(long id);
+    @Autowired
+    CourseDao courseDao;
 
-    Course addCourse(Course course);
+    public List<Course> getCourses() {
+        return courseDao.findAll();
+    }
 
-    Course updateCourse(Course updatedCourse);
+    public Optional<Course> getCourse(long id) {
 
-    String deleteCourse(long id);
+        return courseDao.findById(Long.parseLong(String.valueOf(id)));
+
+    }
+
+    public Course addCourse(Course course) {
+        courseDao.save(course);
+        return course;
+    }
+
+    public Course updateCourse(Course updatedCourse) {
+        Optional<Course>optionalCourse = courseDao.findById(updatedCourse.getId());
+        if (optionalCourse.get()!=null){
+            courseDao.save(updatedCourse);
+            return updatedCourse;
+        }
+        else {
+            return optionalCourse.get();
+        }
+    }
+
+
+    public String deleteCourse(long id) {
+        try {
+            Optional<Course> optionalCourse = courseDao.findById(id);
+            courseDao.delete(optionalCourse.get());
+            return "successfully deleted";
+        } catch (Exception e) {
+            return "Course not found";
+
+        }
+
+    }
+
 }
