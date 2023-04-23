5e2ac3567c397c8fa4b8b6e732c6c62b884441f2
nizamuddinshamrat <nshamrat7@gmail.com>
2023-04-24 04:03:23 +0600

    CommandLineRunner implemented in SpringRestApplication class for printing encrypted password

--- src/main/java/com/nizam/springrest/SpringRestApplication.java
+++ src/main/java/com/nizam/springrest/SpringRestApplication.java
@@ -1,15 +1,30 @@
 package com.nizam.springrest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class SpringRestApplication {
+public class SpringRestApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestApplication.class, args);
-	}
+    @Autowired
+    PasswordEncoder encoder;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApplication.class, args);
+
+    }
+
+    private void passwordEncode() {
+        System.out.println("My custom password is : " + encoder.encode("xyz"));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        passwordEncode();
+    }
 }
 
 
