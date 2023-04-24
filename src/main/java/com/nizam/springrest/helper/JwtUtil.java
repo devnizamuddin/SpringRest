1d7e25d6026d85f4d524a905778cdf29368a1b04
nizamuddinshamrat <nshamrat7@gmail.com>
2023-04-25 02:39:35 +0600

    dependency added for jwt

---
+++ src/main/java/com/nizam/springrest/helper/JwtUtil.java
@@ -0,0 +1,2 @@
+package com.nizam.springrest.helper;public class JwtUtil {
+}
---
+++ src/main/java/com/nizam/springrest/service/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username.equals("nizam")) {
+            return new User("nizam", "xyz", new ArrayList<GrantedAuthority>());
+        } else {
+            throw new UsernameNotFoundException("user not Found");
+        }
+    }
+}
