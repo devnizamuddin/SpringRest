009d2af122ea3a592118648353ec416478fe5575
nizamuddinshamrat <nshamrat7@gmail.com>
2023-04-26 03:35:22 +0600

    course related data removed

--- src/main/java/com/nizam/springrest/controller/GreetingController.java
+++ src/main/java/com/nizam/springrest/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.nizam.springrest;
+package com.nizam.springrest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
     @GetMapping("/getGreetings")
-    public String getGreetings(){
-
-        return "Assalamualikum";
+    public String getGreetings() {
+        return "Hello World";
     }
 }
--- src/main/java/com/nizam/springrest/dao/CourseDao.java
+++
@@ -1,7 +0,0 @@
-package com.nizam.springrest.dao;
-
-import com.nizam.springrest.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-}
