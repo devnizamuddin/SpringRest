4f6f580981e67c28f5ee977958a2b452fe204ed7
nizamuddinshamrat <nshamrat7@gmail.com>
2023-04-26 03:39:48 +0600

    structure changed

--- src/main/java/com/nizam/springrest/services/StorageService.java
+++ src/main/java/com/nizam/springrest/services/StorageService.java
@@ -1,12 +1,34 @@
 package com.nizam.springrest.services;
 
+import com.nizam.springrest.dao.StorageDao;
+import com.nizam.springrest.entities.ImageData;
+import com.nizam.springrest.utils.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
-public interface StorageService {
+@Service
+public class StorageService {
 
-    String uploadImage(MultipartFile file) throws IOException;
+    @Autowired
+    StorageDao storageDao;
 
-    byte[] downloadImage(String fileName);
+    public String uploadImage(MultipartFile file) throws IOException {
+        ImageData imageData = storageDao.save(ImageData.builder().name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtils.compressImage(file.getBytes())).build());
+        if (imageData != null) {
+            return "File uploaded successfully : " + file.getOriginalFilename();
+        }
+        return null;
+    }
+
+    public byte[] downloadImage(String fileName) {
+        Optional<ImageData> dbImageData = storageDao.findByName(fileName);
+        byte[] images = ImageUtils.decompressImage(dbImageData.get().getImageData());
+        return images;
+    }
 }
